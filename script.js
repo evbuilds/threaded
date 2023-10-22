9289f74ee679e4119f5c8fc3a39f019b653c9df0
evbuilds <148723601+evbuilds@users.noreply.github.com>
2023-10-22 18:20:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,31 @@
-let isResizing = false;
-let isDragging = false;
-let startX, startY, startWidth, startHeight;
+const macWindow = document.querySelector('.mac-window');
+const windowHeader = macWindow.querySelector('.window-header');
+const closeButton = macWindow.querySelector('.close');
+const maximizeButton = macWindow.querySelector('.maximize');
 
-const macWindow = document.getElementById('macWindow');
-const windowHeader = document.getElementById('windowHeader');
-const closeButton = document.getElementById('closeButton');
-const maximizeButton = document.getElementById('maximizeButton');
+let [isDragging, isMaximized, startX, startY] = [false, false, 0, 0];
 
-// Dragging
-windowHeader.addEventListener('mousedown', (e) => {
+windowHeader.addEventListener('mousedown', e => {
     isDragging = true;
-    startX = e.clientX - macWindow.getBoundingClientRect().left;
-    startY = e.clientY - macWindow.getBoundingClientRect().top;
+    const rect = macWindow.getBoundingClientRect();
+    [startX, startY] = [e.clientX - rect.left, e.clientY - rect.top];
 });
 
-document.addEventListener('mousemove', (e) => {
+document.addEventListener('mousemove', e => {
     if (!isDragging) return;
-    macWindow.style.left = e.clientX - startX + 'px';
-    macWindow.style.top = e.clientY - startY + 'px';
+    macWindow.style.left = `${e.clientX - startX}px`;
+    macWindow.style.top = `${e.clientY - startY}px`;
 });
 
-document.addEventListener('mouseup', () => {
-    isDragging = false;
-});
+document.addEventListener('mouseup', () => isDragging = false);
 
-// Close window
-closeButton.addEventListener('click', () => {
-    macWindow.style.display = 'none';
-});
+closeButton.addEventListener('click', () => macWindow.style.display = 'none');
 
-// Maximize/Restore window
-let isMaximized = false;
 maximizeButton.addEventListener('click', () => {
-    if (!isMaximized) {
-        macWindow.style.top = '0';
-        macWindow.style.left = '0';
-        macWindow.style.width = '100vw';
-        macWindow.style.height = '100vh';
-        isMaximized = true;
+    isMaximized = !isMaximized;
+    if (isMaximized) {
+        Object.assign(macWindow.style, { top: '0', left: '0', width: '100vw', height: '100vh', transform: '' });
     } else {
-        macWindow.style.width = '300px';
-        macWindow.style.height = '400px';
-        macWindow.style.top = '50%';
-        macWindow.style.left = '50%';
-        macWindow.style.transform = 'translate(-50%, -50%)';
-        isMaximized = false;
+        Object.assign(macWindow.style, { width: '300px', height: '400px', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' });
     }
 });
-
-// This is just a basic setup. You can expand upon this with more features, animations, etc.
-
