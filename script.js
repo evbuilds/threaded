5ba5facdf6242ffe6c99882e454cf73fb57ca80d
evbuilds <148723601+evbuilds@users.noreply.github.com>
2023-11-22 11:21:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,52 @@
-document.addEventListener('DOMContentLoaded', () => {
-  // Function to toggle dark mode
-  function toggleDarkMode() {
-    document.body.classList.toggle('dark-mode');
-  }
+// Function to toggle dark mode
+function toggleDarkMode() {
+  document.body.classList.toggle('dark-mode');
+}
 
-  // Event listener for keyboard shortcut
-  document.addEventListener('keydown', (e) => {
-    // Toggle dark mode when 'D' key is pressed
-    if (e.key === 'd' || e.key === 'D') {
-      toggleDarkMode();
+// Placeholder function for closing a window
+function closeWindow(windowId) {
+  const windowElement = document.getElementById(windowId);
+  if (windowElement) {
+    windowElement.style.display = 'none';
+  }
+}
+
+// Placeholder function for maximizing a window
+function maximizeWindow(windowId) {
+  const windowElement = document.getElementById(windowId);
+  if (windowElement) {
+    windowElement.classList.toggle('maximized');
+  }
+}
+
+// Function to apply square resizing
+function applySquareResizing(windowElement) {
+  const content = windowElement.querySelector('.content');
+  const resizeObserver = new ResizeObserver(entries => {
+    for (let entry of entries) {
+      // Set height equal to the current width to maintain 1:1 aspect ratio
+      const squareSize = `${entry.contentRect.width}px`;
+      windowElement.style.height = squareSize;
+      content.style.height = squareSize; // Apply the same to the content div if needed
     }
   });
+
+  resizeObserver.observe(content); // Observe the `.content` div as it will be resized
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const windows = document.querySelectorAll('.window');
-
-  // Function to apply square resizing
-  function applySquareResizing(windowElement) {
-    const content = windowElement.querySelector('.content');
-    const resizeObserver = new ResizeObserver(entries => {
-      for (let entry of entries) {
-        // Set height equal to the current width to maintain 1:1 aspect ratio
-        const squareSize = `${entry.contentRect.width}px`;
-        windowElement.style.height = squareSize;
-        content.style.height = squareSize; // Apply the same to the content div if needed
-      }
-    });
-
-    resizeObserver.observe(content); // Observe the `.content` div as it will be resized
-  }
 
   // Apply square resizing to each window
   windows.forEach(window => {
     applySquareResizing(window);
-
-    // Your existing code for draggable functionality
+    
+    // Event listeners for window title bar actions
     const titleBar = window.querySelector('.title-bar');
+    const closeButton = window.querySelector('.close');
+    const maximizeButton = window.querySelector('.maximize');
+    
+    // Draggable functionality
     titleBar.onmousedown = function (e) {
       let offsetX = e.clientX - parseInt(window.style.left, 10);
       let offsetY = e.clientY - parseInt(window.style.top, 10);
@@ -52,18 +64,23 @@ document.addEventListener('DOMContentLoaded', () => {
       document.addEventListener('mousemove', mouseMoveHandler);
       document.addEventListener('mouseup', mouseUpHandler);
     };
-
-    window.querySelector('.close').onclick = function () {
-      window.style.display = 'none';
+    
+    // Close window functionality
+    closeButton.onclick = function () {
+      closeWindow(window.id);
     };
-
-    window.querySelector('.maximize').onclick = function () {
-      window.classList.toggle('maximized');
-      if (window.classList.contains('maximized')) {
-        // Apply maximize logic here
-      } else {
-        // Apply restore logic here
-      }
+    
+    // Maximize window functionality
+    maximizeButton.onclick = function () {
+      maximizeWindow(window.id);
     };
   });
+
+  // Event listener for keyboard shortcut to toggle dark mode
+  document.addEventListener('keydown', (e) => {
+    if (e.key === 'd' || e.key === 'D') {
+      toggleDarkMode();
+    }
+  });
 });
+
