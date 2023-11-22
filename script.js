5c9deb4cdbd74689be6629747fad0a090a6eb60c
evbuilds <148723601+evbuilds@users.noreply.github.com>
2023-11-22 10:50:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
   const windows = document.querySelectorAll('.window');
 
-  windows.forEach(window => {
-    const titleBar = window.querySelector('.title-bar');
+  // Function to apply square resizing
+  function applySquareResizing(windowElement) {
+    const content = windowElement.querySelector('.content');
+    const resizeObserver = new ResizeObserver(entries => {
+      for (let entry of entries) {
+        // Set height equal to the current width to maintain 1:1 aspect ratio
+        const squareSize = `${entry.contentRect.width}px`;
+        windowElement.style.height = squareSize;
+        content.style.height = squareSize; // Apply the same to the content div if needed
+      }
+    });
 
+    resizeObserver.observe(content); // Observe the `.content` div as it will be resized
+  }
+
+  // Apply square resizing to each window
+  windows.forEach(window => {
+    applySquareResizing(window);
+
+    // Your existing code for draggable functionality
+    const titleBar = window.querySelector('.title-bar');
     titleBar.onmousedown = function (e) {
       let offsetX = e.clientX - parseInt(window.style.left, 10);
       let offsetY = e.clientY - parseInt(window.style.top, 10);
@@ -28,6 +46,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
     window.querySelector('.maximize').onclick = function () {
       window.classList.toggle('maximized');
+      if (window.classList.contains('maximized')) {
+        // Apply maximize logic here
+      } else {
+        // Apply restore logic here
+      }
     };
   });
 });
