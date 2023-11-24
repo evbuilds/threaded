58159f7dc41cfcb88c9d449488366658003e4eef
evbuilds <148723601+evbuilds@users.noreply.github.com>
2023-11-24 15:32:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,103 @@
-// Global variables for video and canvas elements
 let video;
-let canvases = [];
+let videoPlaying = false;
+let videos = [];
 
-// Toggle video display on and off
-function toggleVideo() {
-    if (video) {
-        if (video.playing) {
-            video.pause();
-            canvases.forEach(cnv => cnv.hide());
-        } else {
-            video.loop();
-            canvases.forEach(cnv => cnv.show());
-        }
-    }
-}
-
-// p5.js sketch
-function makeSketch(windowDiv) {
-    return function (p) {
-        let video;
-
-        p.setup = function () {
-            let cnv = p.createCanvas(windowDiv.offsetWidth, windowDiv.offsetHeight);
-            cnv.parent(windowDiv);
-            video = p.createCapture(p.VIDEO);
-            video.size(cnv.width, cnv.height);
-            video.hide();
-            canvases.push(cnv);
-        };
-
-        p.draw = function () {
-            p.background(0);
-            video.loadPixels();
-            // Draw the video onto the canvas
-            p.image(video, 0, 0, p.width, p.height);
-        };
-    };
-}
-
-// Initialize p5 sketches and video
 function setup() {
-    // Grab all the window content divs
-    const windows = document.querySelectorAll('.window .content');
-    windows.forEach(windowDiv => {
-        new p5(makeSketch(windowDiv), windowDiv);
-    });
+  noCanvas();
+  const windows = document.querySelectorAll('.window .content');
+
+  windows.forEach((windowContent, index) => {
+    let sketch = function (p) {
+      let video;
+      p.setup = function () {
+        p.createCanvas(320, 240).parent(windowContent);
+        video = p.createCapture(p.VIDEO);
+        video.size(320, 240);
+        video.hide();
+        videos.push(video);
+      };
+
+      p.draw = function () {
+        if (videoPlaying) {
+          p.background(0);
+          p.image(video, 0, 0, p.width, p.height);
+        }
+      };
+    };
+    new p5(sketch);
+  });
 }
 
-// Setup key press event listener
-document.addEventListener('keydown', (e) => {
-    if (e.key === 'v' || e.key === 'V') {
-        toggleVideo();
+function toggleVideo() {
+  videoPlaying = !videoPlaying;
+  videos.forEach(video => {
+    if (videoPlaying) {
+      video.loop();
+    } else {
+      video.pause();
     }
+  });
+  
+  document.querySelectorAll('.window .content img').forEach(img => {
+    img.style.display = videoPlaying ? 'none' : 'block';
+  });
+}
+
+function toggleDarkMode() {
+  document.body.classList.toggle('dark-mode');
+}
+
+function closeWindow(windowId) {
+  const windowElement = document.getElementById(windowId);
+  if (windowElement) {
+    windowElement.style.display = 'none';
+  }
+}
+
+function maximizeWindow(windowId) {
+  const windowElement = document.getElementById(windowId);
+  if (windowElement) {
+    windowElement.classList.toggle('maximized');
+  }
+}
+
+function applySquareResizing(windowElement) {
+  const content = windowElement.querySelector('.content');
+  const resizeObserver = new ResizeObserver(entries => {
+    for (let entry of entries) {
+      const squareSize = `${entry.contentRect.width}px`;
+      windowElement.style.height = squareSize;
+      content.style.height = squareSize;
+    }
+  });
+  resizeObserver.observe(content);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const windows = document.querySelectorAll('.window');
+
+  windows.forEach(window => {
+    applySquareResizing(window);
+
+    const closeButton = window.querySelector('.close');
+    const maximizeButton = window.querySelector('.maximize');
+    
+    closeButton.onclick = function () {
+      closeWindow(window.id);
+    };
+
+    maximizeButton.onclick = function () {
+      maximizeWindow(window.id);
+    };
+  });
+
+  document.addEventListener('keydown', (e) => {
+    if (e.key === 'd' || e.key === 'D') {
+      toggleDarkMode();
+    } else if (e.key === 'v' || e.key === 'V') {
+      toggleVideo();
+    }
+  });
+
+  setup(); // Initialize p5 sketches and video
 });
-
-// Initialize the sketches after the window has loaded
-window.onload = setup;
-
-
-
