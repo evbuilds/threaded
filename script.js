0434f9469305c3ccd087cf6389d020fad6f94d34
evbuilds <148723601+evbuilds@users.noreply.github.com>
2023-11-24 14:37:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,114 +1,61 @@
-:root {
-  --background-color-light: #f0f0f0; /* Light mode background */
-  --window-shadow-light: 0 4px 8px rgba(0, 0, 0, 0.1); /* Light mode window shadow */
-  --window-background-light: #ffffff; /* Light mode window background */
-  --title-bar-background-light: #e1e1e1; /* Light mode title bar background */
-  --button-close-light: #ff5f57; /* Light mode close button color */
-  --button-minimize-light: #ffbd2e; /* Light mode minimize button color */
-  --button-maximize-light: #27c93f; /* Light mode maximize button color */
+// Global variables for video and canvas elements
+let video;
+let canvases = [];
 
-  --background-color-dark: #1e1e1e; /* Dark mode background */
-  --window-shadow-dark: 0 4px 8px rgba(0, 0, 0, 0.5); /* Dark mode window shadow */
-  --window-background-dark: #333; /* Dark mode window background */
-  --title-bar-background-dark: #555; /* Dark mode title bar background */
-  --button-close-dark: #e84118; /* Dark mode close button color */
-  --button-minimize-dark: #e1b12c; /* Dark mode minimize button color */
-  --button-maximize-dark: #4cd137; /* Dark mode maximize button color */
+// Toggle video display on and off
+function toggleVideo() {
+    if (video) {
+        if (video.playing) {
+            video.pause();
+            canvases.forEach(cnv => cnv.hide());
+        } else {
+            video.loop();
+            canvases.forEach(cnv => cnv.show());
+        }
+    }
 }
 
-body {
-  background-color: var(--background-color-light);
-  transition: background-color 0.3s;
+// p5.js sketch
+function makeSketch(windowDiv) {
+    return function (p) {
+        let video;
+
+        p.setup = function () {
+            let cnv = p.createCanvas(windowDiv.offsetWidth, windowDiv.offsetHeight);
+            cnv.parent(windowDiv);
+            video = p.createCapture(p.VIDEO);
+            video.size(cnv.width, cnv.height);
+            video.hide();
+            canvases.push(cnv);
+        };
+
+        p.draw = function () {
+            p.background(0);
+            video.loadPixels();
+            // Draw the video onto the canvas
+            p.image(video, 0, 0, p.width, p.height);
+        };
+    };
 }
 
-.desktop {
-  position: relative;
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  flex-wrap: wrap;
-  align-items: flex-start;
-  justify-content: center;
-  padding: 10px;
-  box-sizing: border-box;
+// Initialize p5 sketches and video
+function setup() {
+    // Grab all the window content divs
+    const windows = document.querySelectorAll('.window .content');
+    windows.forEach(windowDiv => {
+        new p5(makeSketch(windowDiv), windowDiv);
+    });
 }
 
-.window {
-  width: calc(50% - 20px);
-  margin: 10px;
-  border: 1px solid #ccc;
-  background-color: var(--window-background-light);
-  border-radius: 5px;
-  box-shadow: var(--window-shadow-light);
-  overflow: hidden;
-  position: relative;
-  padding-top: calc(50% - 20px);
-  box-sizing: border-box;
-  transition: background-color 0.3s, box-shadow 0.3s;
-}
+// Setup key press event listener
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'v' || e.key === 'V') {
+        toggleVideo();
+    }
+});
 
-.title-bar {
-  cursor: grab;
-  background-color: var(--title-bar-background-light);
-  padding: 6px 10px;
-  border-top-left-radius: 4px;
-  border-top-right-radius: 4px;
-  position: absolute;
-  width: 100%;
-  top: 0;
-  z-index: 1;
-  transition: background-color 0.3s;
-}
+// Initialize the sketches after the window has loaded
+window.onload = setup;
 
-.title-bar-buttons {
-  display: flex;
-}
-
-.button {
-  width: 12px;
-  height: 12px;
-  border-radius: 50%;
-  margin-left: 4px;
-  transition: background-color 0.3s;
-}
-
-.close { background-color: var(--button-close-light); }
-.minimize { background-color: var(--button-minimize-light); }
-.maximize { background-color: var(--button-maximize-light); }
-
-.content {
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.content img {
-  max-width: 100%;
-  max-height: 100%;
-  object-fit: cover;
-}
-
-/* Dark mode specific styles */
-body.dark-mode {
-  background-color: var(--background-color-dark);
-}
-
-body.dark-mode .window {
-  background-color: var(--window-background-dark);
-  box-shadow: var(--window-shadow-dark);
-}
-
-body.dark-mode .title-bar {
-  background-color: var(--title-bar-background-dark);
-}
-
-body.dark-mode .button.close { background-color: var(--button-close-dark); }
-body.dark-mode .button.minimize { background-color: var(--button-minimize-dark); }
-body.dark-mode .button.maximize { background-color: var(--button-maximize-dark); }
 
 
